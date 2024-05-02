005c71596e4af77513c2d70e8c2014f57dbd3ee0
QoyyumO <oyinlolaoyadeyi@gmail.com>
2024-05-02 16:49:08 +0100

    Comparison Operators

--- index.js
+++ index.js
@@ -28,3 +28,15 @@ lastName = "Oyadeyi";
 fullName = firstName + " " + lastName; //Concatenate firstName, whitespace and lastName 
 console.log(fullName); //This should output the full name Abdul-Qoyyum Oyadeyi. 
 
+//Comparison Operators
+
+a = 10; 
+a < 11; //Returns true 
+console.log(a < 11); //Displays true in console 
+var test = a > 20; //Declares a variable named test and gives it the value of false. 
+console.log(test); //Displays false in console 
+a >= 10; //Returns true 
+a <= 20; //Returns true 
+a == 10; //Returns true 
+a === "10"; //Returns false because a holds an integer value of 10 and not string value "10"
+
