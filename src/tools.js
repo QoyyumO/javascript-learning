cbb3b2962a103a61a72e26678a7ac09cc24cfc4e
QoyyumO <oyinlolaoyadeyi@gmail.com>
2024-05-09 15:47:11 +0100

    destructuring assignments 2

--- src/tools.js
+++ src/tools.js
@@ -13,7 +13,7 @@ export const dummyFunction2 = () => {
 
 export const genericFunction = () => { 
     let languages = ['Python', 'JavaScript', 'Java', 'C#', 'C++']; //declare an array of elements 
-    let [firstLang, secondLang] = languages; //assign first element in languages array to firstLang and second element to secondLang. 
-    return `First language is ${firstLang} and the second is ${secondLang}` 
+    let [firstLang, secondLang, ...otherLanguages] = languages; //otherLanguages will be equal to ['Java', 'C#', 'C++'] 
+    return `First language is ${firstLang} and the second is ${secondLang}. The rest are ${otherLanguages}` 
     } 
 export default logger; 
