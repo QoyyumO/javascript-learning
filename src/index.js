475a09c85651ab393e67384dfd79b9e2e2a9df69
QoyyumO <oyinlolaoyadeyi@gmail.com>
2024-05-09 14:39:45 +0100

    adding line break

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import log, {appName, dummyFunction} from './tools.js';
 
 log ("I am logging using tools module");
 
-log("Welcome! The application name is "+ appName + ". There is a function that returns '" + dummyFunction()+ "'")
+log("Welcome! The application name is "+ appName + ". \n\
+There is a function that returns '" + dummyFunction()+ "'")
