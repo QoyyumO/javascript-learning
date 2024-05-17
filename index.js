299c80f8cb94b8bf816d5ea099b63a17d11a7c1c
QoyyumO <oyinlolaoyadeyi@gmail.com>
2024-05-17 18:35:54 +0100

    Function declaration for group instructions to carry out specific task

--- index.js
+++ index.js
@@ -197,3 +197,29 @@ recursiveReadLineAsync(); //No exit yet. Call self again to ask another question
 }; 
 //Finally, invoke the defined function to start the recursion 
 recursiveReadLineAsync(); 
+
+
+//group instructions to carry out specific task
+//Function declaration
+//By direct declaration 
+function addTwoNumbersA(number1, number2){ 
+    return number1 + number2; //Add the two numbers passed when function is called 
+} 
+//By expression 
+var addTwoNumbersB = function(number1, number2){ 
+  return number1 + number2; 
+} 
+
+/* console.log(addTwoNumbersA(20,30)); //Call addTwoNumbersA and print return value (50). 
+console.log(addTwoNumbersB(20,30)); //Call addTwoNumbersB and print return value (50).  */
+
+console.log(addTwoNumbersA(20,30)); //Call addTwoNumbersA defined below. Correct! 
+function addTwoNumbersA(number1, number2){ 
+return number1 + number2; //Add the two numbers passed when function is called 
+} 
+console.log(addTwoNumbersB(20,30)); //Call addTwoNumbersB defined below. Wrong! 
+var addTwoNumbersB = function(number1, number2){ 
+return number1 + number2; //Add the two numbers passed when function is called 
+} 
+
+
