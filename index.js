62dfe208c4226c8f98db324bdb272c0091b9cab8
QoyyumO <oyinlolaoyadeyi@gmail.com>
2024-05-17 18:37:47 +0100

    Passing arguments to function by value or by reference for group instructions to carry out specific task

--- index.js
+++ index.js
@@ -222,4 +222,14 @@ var addTwoNumbersB = function(number1, number2){
 return number1 + number2; //Add the two numbers passed when function is called 
 } 
 
+//Passing arguments to function by value or by reference 
+//Define a function that receives a number to be squared 
+function square(number){ 
+    number = number**2; 
+    return number; 
+    } 
+    var number = 20; //Declare variable that holds number to be squared 
+    var numberSquared = square(number); //Pass number (by value) to the square() function  
+    console.log(numberSquared); //The function returns 400 
+    console.log(number); //The value of number variable outside the function remains 20 
 
