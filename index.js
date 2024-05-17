978c2044d92a51d85cdb6807f4cad70157ac43c3
QoyyumO <oyinlolaoyadeyi@gmail.com>
2024-05-17 18:43:32 +0100

    accessing arrays, along side array literals

--- index.js
+++ index.js
@@ -277,3 +277,16 @@ console.log(basket); //Prints out the array literal [ ]
 //Declare a variable named myExpressions and assign an array literal which contains expressions. 
 var myExpressions = ["car", 2+3, 10, true];  //The second element includes a plus operation 
 console.log(myExpressions); //Prints out [ 'car', 5, 10, true] 
+
+//Accessing Arrays
+console.log(myExpressions[0]); //Prints out 'car' 
+console.log(myExpressions[1]); //Prints out 5 
+console.log(myExpressions[2]); //Prints out 10 
+console.log(myExpressions[3]); //Prints out true 
+myExpressions[3] = false; //Change the expression at position 3. 
+console.log(myExpressions[3]); //Prints out fals
+
+// Array literals
+
+var myExpressions = ["car", , 10, true];  //The second element (i.e. index position 1), is undefined 
+console.log(myExpressions[1]); //Prints out undefined
