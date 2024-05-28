04327081dcd01869682ddf3fdfb17cebbbb1e343
QoyyumO <oyinlolaoyadeyi@gmail.com>
2024-05-28 18:30:05 +0100

    use the entity from Service class

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentRegistrationModule } from './student-registration/student-registration.module';
 
 @Module({
-  imports: [StudentRegistrationModule],
-  controllers: [AppController],
-  providers: [AppService],
+imports: [TypeOrmModule.forRoot({
+type: "postgres",
+host: "localhost",
+port: 5432,
+username: "postgres",
+password: "postgres",
+database: "nestwafprimer",
+entities: [
+"dist/**/*.entity{.ts,.js}"
+],
+synchronize: true
+}), StudentRegistrationModule],
+controllers: [AppController],
+providers: [AppService],
 })
 export class AppModule {}
